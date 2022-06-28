a8bb2462066f3e3b7215da2f25da4e77c145b556
Angelo Yesid <aymonsalver@unal.edu.co>
2022-06-28 14:40:38 -0500

    Pregunta4_Correccion lista neuronas

--- preguntas.py
+++ preguntas.py
@@ -110,7 +110,7 @@ def pregunta_04():
     #   * Use parada temprana
 
     param_grid = {
-        "mlpregressor__hidden_layer_sizes": list(range(1,9)),  
+        "mlpregressor__hidden_layer_sizes": [1, 2, 3, 4, 5, 6, 7, 8],  
         "mlpregressor__activation": ["relu"],  
         "mlpregressor__learning_rate": ["adaptive"],  
         "mlpregressor__momentum": [0.7, 0.8, 0.9],  
